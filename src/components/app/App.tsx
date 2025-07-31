298e2cff9f547a7bad6691baa9e8d6b65a3dee5b
Alberto Eliseo Pacheco Allende <alberto@skillmil.com>
2025-07-30 22:39:05 -0400

    Adding basic functionality to switch from one view to another

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,11 +6,14 @@ import CartView from '../cart/CartView';
 
 function App() {
 
-  const [isMainViewMode] = useState(false);
+  const [isMainViewMode,setIsMainViewMode] = useState(false);
 
   return (
     <div> 
-      <nav>Navs</nav>
+      <nav>
+        <button onClick={() => setIsMainViewMode(true)}>Home</button>
+        <button onClick={() => setIsMainViewMode(false)}>Cart</button>
+      </nav>
       {isMainViewMode ? <MainView></MainView> : <CartView></CartView>}
     </div>
   )
