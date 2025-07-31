200e1790fc18ec1d944e09e907719c4be8007a8f
Alberto Eliseo Pacheco Allende <alberto@skillmil.com>
2025-07-30 23:09:13 -0400

    Creating some styles for nav bar, Products and cart.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,17 +2,27 @@ import { useState } from 'react';
 import './App.css'
 import MainView from '../main/MainView';
 import CartView from '../cart/CartView';
+import { ShoppingCartIcon } from '@heroicons/react/24/outline'
 
 
 function App() {
 
   const [isMainViewMode,setIsMainViewMode] = useState(false);
 
+  
+
   return (
-    <div> 
-      <nav>
-        <button onClick={() => setIsMainViewMode(true)}>Home</button>
-        <button onClick={() => setIsMainViewMode(false)}>Cart</button>
+    <div className="min-h-screen bg-gray-100"> 
+      <nav  className="bg-white shadow p-4 flex justify-evenly items-center">
+        <button className={`px-4 py-2 rounded ${
+          isMainViewMode ? 'bg-blue-600 text-white' : 'bg-gray-200 text-gray-800'
+          }`} onClick={() => setIsMainViewMode(true)}>
+              Products
+        </button>
+        <button className={`px-4 py-2 rounded ${!isMainViewMode ? 'bg-blue-600 text-white' : 'bg-gray-200 text-gray-800'}`} 
+        onClick={() => setIsMainViewMode(false)}>
+          <ShoppingCartIcon className="h-5 w-5" />
+        </button>
       </nav>
       {isMainViewMode ? <MainView></MainView> : <CartView></CartView>}
     </div>
